b2b9b7bdf68df7c23bbb03919f70b49b0c666a0f
jsanchezp21 <jsanchezpalencia@gmail.com>
2026-05-20 21:43:25 +0200

    Fix api url frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://bodaweb.onrender.com/api',
   weddingDate: '2026-08-29T18:00:00+02:00',
   coupleNames: 'Andrea & Jesús',
   weddingDateText: '29 · 08 · 2026',
